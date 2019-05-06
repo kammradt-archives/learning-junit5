74aece291f16f619744d8502004e82c86fcc481b
kammradt <vinicius.kammradt1@gmail.com>
2019-05-06 14:55:57 -0300

    :white_check_mark: - New tests added.

--- src/test/java/math/MathUtilsTest.java
+++ src/test/java/math/MathUtilsTest.java
@@ -17,20 +17,95 @@ class MathUtilsTest {
 
     @Test
     void testAdd() {
+        double expected = 0;
+        double actual = mathUtils.add(1, -1);
+
+        assertEquals(expected, actual,
+                "Should add two numbers.");
+
+    }
+
+    @Test
+    void testAddPositives() {
         double expected = 2;
         double actual = mathUtils.add(1, 1);
 
         assertEquals(expected, actual,
-                "Should add two numbers.");
-        
+                "Should add two positive numbers.");
+
+    }
+
+    @Test
+    void testAddNegatives() {
+        double expected = -2;
+        double actual = mathUtils.add(-1, -1);
+
+        assertEquals(expected, actual,
+                "Should add two negative numbers.");
+
     }
 
     @Test
     void testSubtract() {
+        double expected = -2;
+        double actual = mathUtils.subtract(-1, 1);
+
+        assertEquals(expected, actual,
+                "Should subtract two numbers.");
+    }
+
+    @Test
+    void testSubtractPositives() {
+        double expected = 0;
+        double actual = mathUtils.subtract(1, 1);
+
+        assertEquals(expected, actual,
+                "Should subtract two positive numbers.");
+    }
+
+    @Test
+    void testSubtractNegatives() {
+        double expected = 0;
+        double actual = mathUtils.subtract(-1, -1);
+
+        assertEquals(expected, actual,
+                "Should subtract two negative numbers.");
     }
 
     @Test
     void testMultiply() {
+        double expected = 10;
+        double actual = mathUtils.multiply(2, 5);
+
+        assertEquals(expected, actual,
+                "Should multiply two numbers.");
+    }
+
+    @Test
+    void testMultiplyByOne() {
+        double expected = 2;
+        double actual = mathUtils.multiply(2, 1);
+
+        assertEquals(expected, actual,
+                "Should multiply by one.");
+    }
+
+    @Test
+    void testMultiplyByZero() {
+        double expected = 0;
+        double actual = mathUtils.multiply(2, 0);
+
+        assertEquals(expected, actual,
+                "Should multiply by zero.");
+    }
+
+    @Test
+    void testMultiplyByNegative() {
+        double expected = -10;
+        double actual = mathUtils.multiply(2, -5);
+
+        assertEquals(expected, actual,
+                "Should multiply by a negative negative.");
     }
 
     @Test
