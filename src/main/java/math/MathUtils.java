b37c7f26903157b8c11c0ed306888bf8be523f99
kammradt <vinicius.kammradt1@gmail.com>
2019-05-06 11:09:46 -0300

    :sparkles: - New tests created.

--- src/main/java/math/MathUtils.java
+++ src/main/java/math/MathUtils.java
@@ -1,8 +1,21 @@
 package math;
 
 public class MathUtils {
-    public int add(int a, int b){
+
+    public double add(double a, double b){
         return a + b;
+    }
+
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
     }
     public double calculateCircleArea(double radius){
         return Math.PI * (radius * radius);
--- src/test/java/math/MathUtilsTest.java
+++ src/test/java/math/MathUtilsTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
-        int expected = 2;
-        int actual = mathUtils.add(1, 1);
+        double expected = 2;
+        double actual = mathUtils.add(1, 1);
 
         assertEquals(expected, actual,
                 "Should add two numbers.");
@@ -18,11 +18,38 @@ class MathUtilsTest {
     }
 
     @Test
-    void computeCircleArea() {
+    void testSubtract() {
+    }
+
+    @Test
+    void testMultiply() {
+    }
+
+    @Test
+    void testDivideByZeroPositiveInfinity() {
+        MathUtils mathUtils = new MathUtils();
+        double expected = Double.POSITIVE_INFINITY;
+        double actual = mathUtils.divide(1,0);
+        assertEquals(expected, actual,
+                "Divide by zero should return positive infinity");
+    }
+
+    @Test
+    void testDivideByZeroNegativeInfinity() {
+        MathUtils mathUtils = new MathUtils();
+        double expected = Double.NEGATIVE_INFINITY;
+        double actual = mathUtils.divide(-1,0);
+        assertEquals(expected, actual,
+                "Divide by zero should return negative infinity");
+    }
+
+    @Test
+    void testComputeCircleArea() {
         MathUtils mathUtils = new MathUtils();
 
         assertEquals(314.1592653589793, mathUtils.calculateCircleArea(10),
                 "Should return right circle area");
 
     }
+
 }
