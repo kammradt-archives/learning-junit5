65887bd28a9897e62c2c8aabd84a69b25874095d
kammradt <vinicius.kammradt1@gmail.com>
2019-05-05 23:27:00 -0300

    :pencil: - Create message to tests.

--- src/main/java/math/MathUtils.java
+++ src/main/java/math/MathUtils.java
@@ -4,4 +4,7 @@ public class MathUtils {
     public int add(int a, int b){
         return a + b;
     }
+    public double calculateCircleArea(double radius){
+        return Math.PI * (radius * radius);
+    }
 }
--- src/test/java/math/MathUtilsTest.java
+++ src/test/java/math/MathUtilsTest.java
@@ -12,6 +12,17 @@ class MathUtilsTest {
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
-        assertEquals(expected, actual, "Should add two numbers.");
+        assertEquals(expected, actual,
+                "Should add two numbers.");
+        
+    }
+
+    @Test
+    void computeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+
+        assertEquals(314.1592653589793, mathUtils.calculateCircleArea(10),
+                "Should return right circle area");
+
     }
 }
